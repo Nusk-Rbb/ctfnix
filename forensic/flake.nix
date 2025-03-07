30e8ffb42678d41d4ea085140e7974492613fa2f
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2025-03-07 19:28:50 +0900

    :bookmark: Completed forensics tools

--- forensic/flake.nix
+++ forensic/flake.nix
@@ -7,7 +7,7 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       });
     in
     {
@@ -23,7 +23,6 @@
               volatility2-bin
               autopsy
               netcat
-              rekall
             ];
           };
       });
