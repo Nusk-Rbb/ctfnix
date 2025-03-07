189329fcadad68d1607b0dbf122bcf4ab0fceab3
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2025-03-07 19:30:14 +0900

    :bookmark: Completed reversing tools

--- rev/flake.nix
+++ rev/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Web environment";
+  description = "A Nix-flake-based Reversing environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
@@ -7,7 +7,7 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       });
     in
     {
